310973a1fdf23209d2de87b0d33784f2541c5b26
Suren2006 <105384966+Suren2006@users.noreply.github.com>
2022-10-02 22:50:13 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
-// navigator.geolocation.getCurrentPosition(mapgeo);
-
-// // function showgeo(position) {
-// //     var x = position.coords.latitude
-// //     var y = position.coords.longitude
-// //     console.log(x + '\n' + y)
-// // }
-
-// function mapgeo(position) {
-//     var x = position.coords.latitude
-//     var y = position.coords.longitude
-//     console.log(x + '\n' + y)
-//     return x, y
-// }
-
-// function opengooglemap() {    
-// }
-
 function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-      document.write("Geolocation is not supported by this browser.");
-    }
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(showPosition);
+} else { 
+document.write("Geolocation is not supported by this browser.");
 }
-  
-  function showPosition(position) {
-    var x = position.coords.latitude
-    var y = position.coords.longitude
-    window.open(`https://www.google.com/maps/@${x},${y},16z`,"_blank")
- }
+}
+
+function showPosition(position) {
+var lat = position.coords.latitude
+var lng = position.coords.longitude
+document.getElementById("iframe").src=`https://maps.google.com/maps?q=${lat},${lng}&t=&z=15&ie=UTF8&iwloc=&output=embed`
+document.getElementById("iframe").style = "display:block"
+}
+
+
