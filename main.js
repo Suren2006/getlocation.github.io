bac5686f8f000ddc6e3abe2d0fd1e9daf81bc6c9
Suren2006 <105384966+Suren2006@users.noreply.github.com>
2022-10-16 20:25:31 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function getLocation() {
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(showPosition);
+} else { 
+document.write("Geolocation is not supported by this browser.");
+}
+}
+
+function showPosition(position) {
+var lat = position.coords.latitude
+var lng = position.coords.longitude
+document.getElementById("iframe").src=`https://maps.google.com/maps?q=${lat},${lng}&t=&z=15&ie=UTF8&iwloc=&output=embed`
+
+// document.getElementById("iframe").src=`https://maps.google.com/maps/dir/${lat},${lng}/${lng},${lat}&t=&z=15&ie=UTF8&iwloc=&output=embed`
+// document.getElementById("iframe").src=`https://www.google.com/maps/dir/${lat},${lng}/${lat},${lng}/@${lat},${lng},17.36z/data=!4m2!4m1!3e2`
+}
+
+
+const successCallback = (position) => {
+    lat2 = position.coords.latitude
+    lng2 = position.coords.longitude
+    document.getElementById("iframe2").src=`https://maps.google.com/maps?q=${lat2},${lng2}&t=&z=15&ie=UTF8&iwloc=&output=embed`
+
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+
+const id = navigator.geolocation.watchPosition(successCallback, errorCallback);
+
