4b64c490a608a758e0e37c26d858ab39721feeeb
Suren2006 <105384966+Suren2006@users.noreply.github.com>
2022-10-16 20:30:47 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -15,17 +15,18 @@ document.getElementById("iframe").src=`https://maps.google.com/maps?q=${lat},${l
 // document.getElementById("iframe").src=`https://www.google.com/maps/dir/${lat},${lng}/${lat},${lng}/@${lat},${lng},17.36z/data=!4m2!4m1!3e2`
 }
 
+function showPosition2() {
+    const successCallback = (position) => {
+        lat2 = position.coords.latitude
+        lng2 = position.coords.longitude
+        document.getElementById("iframe2").src=`https://maps.google.com/maps?q=${lat2},${lng2}&t=&z=15&ie=UTF8&iwloc=&output=embed`
 
-const successCallback = (position) => {
-    lat2 = position.coords.latitude
-    lng2 = position.coords.longitude
-    document.getElementById("iframe2").src=`https://maps.google.com/maps?q=${lat2},${lng2}&t=&z=15&ie=UTF8&iwloc=&output=embed`
+    };
+    
+    const errorCallback = (error) => {
+        console.log(error);
+    };
 
-  };
-  
-  const errorCallback = (error) => {
-    console.log(error);
-  };
+    const id = navigator.geolocation.watchPosition(successCallback, errorCallback);
 
-const id = navigator.geolocation.watchPosition(successCallback, errorCallback);
-
+}
